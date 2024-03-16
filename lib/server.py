c1dc439366974f9c0b6dfcbcf4a9bb0af433d148
Charlie <gamerboy030607@gmail.com>
2024-03-16 19:20:19 +1100

    Add support for executing C++ and Node.js code

--- lib/server.py
+++ lib/server.py
@@ -8,9 +8,20 @@ import re
 WS_HOST = "127.0.0.1"
 WS_PORT = 5000
 ws_port2 = 5001
+ws_port3 = 5002
+ws_port4 = 5003
+ws_port5 = 5004
+ws_port6 = 5005
+ws_port7 = 5006
+ws_port8 = 5007
+ws_port9 = 5008
+ws_port10 = 5009
+
 SHELL = "/bin/bash"
 TEMP_PYTHON_FILE = "code/temp.py"
 TEMP_BASH_FILE = "code/temp.sh"
+TEMP_CPP_FILE = "code/temp.cpp"
+TEMP_NODE_FILE = "code/temp.js"
 DIRECTORY= "code"
 if not os.path.exists(DIRECTORY):
     os.makedirs(DIRECTORY)
@@ -23,7 +34,56 @@ def translate_terminal_colors(code):
         '4': 'blue',
         '5': 'magenta',
         '6': 'cyan',
-        '7': 'white'
+        '7': 'white',
+        '8': 'black',
+        '9': 'red',
+        '10': 'green',
+        '11': 'yellow',
+        '12': 'blue',
+        '13': 'magenta',
+        '14': 'cyan',
+        '15': 'white',
+        '9': 'red',
+        '10': 'green',
+        '11': 'yellow',
+        '12': 'blue',
+        '13': 'magenta',
+        '14': 'cyan',
+        '15': 'white',
+        '30': 'black',
+        '31': 'red',
+        '32': 'green',
+        '33': 'yellow',
+        '34': 'blue',
+        '35': 'magenta',
+        '36': 'cyan',
+        '37': 'white',
+        '100': 'black',
+        '101': 'red',
+        '110': 'green',
+        '111': 'yellow',
+        '112': 'blue',
+        '113': 'magenta',
+        '114': 'cyan',
+        '115': 'white',
+        '40': 'black',
+        '41': 'red',
+        '42': 'green',
+        '43': 'yellow',
+        '44': 'blue',
+        '45': 'magenta',
+        '46': 'cyan',
+        '47': 'white',
+        '100': 'black',
+        '101': 'red',
+        '102': 'green',
+        '103': 'yellow',
+        '104': 'blue',
+        '105': 'magenta',
+        '106': 'cyan',
+        '107': 'white',
+        '108': 'black'
+    
     }
     
     translated_code = ''
@@ -46,7 +106,64 @@ def translate_terminal_colors(code):
         i += 1
     
     return translated_code
+### START
+async def execute_CPP(code, websocket):
+    with open(TEMP_CPP_FILE, 'w') as file:
+        file.write(code)
+    
+    child = pexpect.spawn(f"g++ {TEMP_CPP_FILE} -o temp", encoding="utf-8")
 
+    while True:
+        try:
+            index = child.expect(['\n', pexpect.EOF, pexpect.TIMEOUT], timeout=1)
+            if index == 0:
+                coded_text = translate_terminal_colors(child.before)
+                await websocket.send(coded_text)
+            elif index == 1:
+                coded_text = translate_terminal_colors(child.before)
+                await websocket.send(coded_text)
+                break
+        except pexpect.exceptions.TIMEOUT:
+            break
+    os.remove(TEMP_CPP_FILE)
+
+async def CPP(websocket, path):
+    try:
+        
+        async for code in websocket:
+            await execute_CPP(code, websocket)
+    except websockets.exceptions.ConnectionClosedOK:
+        pass
+#### END
+### START
+async def execute_NODE(code, websocket):
+    with open(TEMP_NODE_FILE, 'w') as file:
+        file.write(code)
+    
+    child = pexpect.spawn(f"node {TEMP_NODE_FILE}", encoding="utf-8")
+
+    while True:
+        try:
+            index = child.expect(['\n', pexpect.EOF, pexpect.TIMEOUT], timeout=1)
+            if index == 0:
+                coded_text = translate_terminal_colors(child.before)
+                await websocket.send(coded_text)
+            elif index == 1:
+                coded_text = translate_terminal_colors(child.before)
+                await websocket.send(coded_text)
+                break
+        except pexpect.exceptions.TIMEOUT:
+            break
+    os.remove(TEMP_NODE_FILE)
+
+async def NODE(websocket, path):
+    try:
+        async for code in websocket:
+            await execute_NODE(code, websocket)
+    except websockets.exceptions.ConnectionClosedOK:
+        pass
+#### END
+### START
 async def execute_shell(code, websocket):
     with open(TEMP_BASH_FILE, 'w') as file:
         file.write(code)
@@ -73,7 +190,7 @@ async def shell(websocket, path):
             await execute_shell(code, websocket)
     except websockets.exceptions.ConnectionClosedOK:
         pass
-
+#### END
 
 async def execute_code(code, websocket):
     with open(TEMP_PYTHON_FILE, 'w') as file:
@@ -103,8 +220,16 @@ async def server(websocket, path):
 
 ws_server = websockets.serve(server, WS_HOST, WS_PORT)
 ws_shell = websockets.serve(shell, WS_HOST, ws_port2)
+ws_node = websockets.serve(NODE, WS_HOST, ws_port3)
+ws_cpp = websockets.serve(CPP, WS_HOST, ws_port4)
+
 print("Server started at port", WS_PORT)
 print("Shell started at port", ws_port2)
+print("Node started at port", ws_port3)
+print("C++ started at port", ws_port4)
+
 asyncio.get_event_loop().run_until_complete(ws_shell)
 asyncio.get_event_loop().run_until_complete(ws_server)
+asyncio.get_event_loop().run_until_complete(ws_node)
+asyncio.get_event_loop().run_until_complete(ws_cpp)
 asyncio.get_event_loop().run_forever()
