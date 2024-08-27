ef494439a27b5a0368b833a54c999ea9c80a121c
charliesan <charliesan@envs.net>
2024-08-27 20:24:31 +1000

    Refactor code and remove unused files

--- runner/lib/PMSSystem/PMSsystem.py
+++ runner/lib/PMSSystem/PMSsystem.py
@@ -6,22 +6,25 @@ import asyncio
 import re
 import sys
 from config import config
-
-
+import threading
+import subprocess
+import asyncio
+import threading
 ### START
-async def execute_code(cmmand, websocket):
+async def execute_code(command, websocket):
+    print("Executing code")
+    print("Command: " + command)
     child = pexpect.spawn(command, encoding="utf-8")
     while True:
         try:
             index = child.expect(['\n', '.', pexpect.EOF, pexpect.TIMEOUT], timeout=1)
             if index == 0 or index == 1:
-                #coded_text = translate_terminal_colors(child.before)
-                await websocket.send(child.after)
+                decolorised_text = re.sub(r'\x1b\[[0-9;]*m', '', child.after)
+                await websocket.send(decolorised_text)
             elif index == 2:
                 break
         except pexpect.exceptions.TIMEOUT:
             break
-    os.remove(TEMP_BASH_FILE)
 
 async def Write_code_Buffer(websocket, path):
     print("PMS Code")
@@ -29,102 +32,158 @@ async def Write_code_Buffer(websocket, path):
     print("Websocket: " + str(websocket))
     while True:
         code = await websocket.recv()
-        parsed_code = json.loads(code)
-        print("Code: " + str(parsed_code))
-        filename = parsed_code["File_Name"]
-        filecontents = parsed_code["File_Contents"]
-        # get the file extention from the name 
-        fileext = re.split(r"\.", filename)[1]
-        with open("code/" + filename, "w") as f:
-            f.write(filecontents)
-        await websocket.send("File saved successfully " + filename)
+        # we got the code now, just got to check the first line for the project name in the languages comment
+        # then we can save the code to the project directory
         
-
-async def Read_PMS_File(websocket, path):
+        # get the first line of the code
+        first_line = code.split("\n")[0]
+        print("First Line: " + first_line)
+        print("seccond Line: " + code.split("\n")[1])
+        print("Third Line: " + code.split("\n")[2])
+        # check if the first line is a comment
+        if first_line.startswith("#") or first_line.startswith("//") or first_line.startswith("/*"):
+            if re.search("File_name: ", code.split("\n")[1]): # check if the comment has the filename in it
+                # check if the comment has the project name in it
+                if re.search("Project: ", code.split("\n")[2]):
+                    # get the project name from the comment
+                    project_name = code.split("\n")[2].split("Project: ")[1]
+                    File_name = code.split("\n")[1].split("File_name: ")[1]
+                    print("Project Name: " + project_name)
+                    print("File Name: " + File_name)
+                    # find the project directory if it exists
+                    if os.path.exists(project_name):
+                        # save the code to the project directory
+                        with open(project_name + "/" + File_name, "w") as f:
+                            f.write(code)
+                            print("Code saved successfully")
+                        await websocket.send("Code saved successfully\n")
+                    else:
+                        await websocket.send("<color=red>Error:</color> Project directory not found.\n")
+                        # create the project directory
+                        os.system("mkdir " + project_name)
+                        await websocket.send("Project directory created")
+                        # save the code to the project directory
+                        with open(project_name + "/" + File_name, "w") as f:
+                            f.write(code)
+                            print("Code saved successfully\n")
+                        await websocket.send("Code saved successfully\n")
+                        
+                        
+                else:
+                    await websocket.send("<color=red>Error:</color> Project name and filename not found in comment\n")
+            else: 
+                await websocket.send("<color=red>Error:</color> Filename not found in comment\n")
+        else:
+            await websocket.send("<color=red>Error:</color> Comment not found in code\n")
+            
+            
+            
+async def Read_PMS_File(websocket, path,code):
     print("PMS File")
     print("Path: " + path)
     print("Websocket: " + str(websocket))
-    while True:
-        code = await websocket.recv()
-        parsed_code = json.loads(code)
-        print("Code: " + str(parsed_code))
-        Sysver = parsed_code["PMS_System"]
-        Project_name = parsed_code["Project_Name"]
-        Entry_point = parsed_code["Main_File"]
-        Output_Name = parsed_code["Project_Output"]
-        Project_Build_System = parsed_code["Project_Build_System"]
-        print("System Version: " + Sysver)
-        print("Project Name: " + Project_name)
-        print("Entry Point: " + Entry_point)
-        print("Output Name: " + Output_Name)
-        print("Project Build System: " + Project_Build_System)
+ 
+      
+    parsed_code = json.loads(code)
+    print("Code: " + str(parsed_code))
+    Sysver = parsed_code["PMS_System"]
+    Project_name = parsed_code["Project_Name"]
+    Entry_point = parsed_code["Main_File"]
+    Output_Name = parsed_code["Project_Output"]
+    Project_Build_System = parsed_code["Project_Build_Systems"]
+    print("System Version: " + Sysver + "\n")
+    print("Project Name: " + Project_name + "\n")
+    print("Entry Point: " + Entry_point + "\n")
+    print("Output Name: " + Output_Name + "\n")
+    print("Project Build System: " + Project_Build_System + "\n")
+    
+    # Create the project directory
+    os.system("mkdir " + Project_name)
+    # Copy the files to the project directory
+    os.system("cp code/* " + Project_name)
+    
+    # make a list of the varibles we parsed from the JSON
+    project_vars = [Sysver, Project_name, Entry_point, Output_Name, Project_Build_System]
+    # write the project vars to a file
+    with open(Project_name + "/project_vars.json", "w") as f:
+        f.write(json.dumps(project_vars))
         
-        # Create the project directory
-        os.system("mkdir " + Project_name)
-        # Copy the files to the project directory
-        os.system("cp code/* " + Project_name)
-        
-        # make a list of the varibles we parsed from the JSON
-        project_vars = [Sysver, Project_name, Entry_point, Output_Name, Project_Build_System]
-        # write the project vars to a file
-        with open(Project_name + "/project_vars.json", "w") as f:
-            f.write(json.dumps(project_vars))
-            
-        await websocket.send("Project saved successfully " + Project_name)
-        await websocket.send("Project vars saved successfully " + Project_name)
-        await websocket.send("Running PMS System")
-        Run_PMS_system()
+    await websocket.send("Project saved successfully " + Project_name + "\n" )
+    await websocket.send("Project vars saved successfully " + Project_name + "\n" )
+    await websocket.send("Running PMS System\n" )
+    await Run_PMS_system(websocket,path,Project_name)
         
         
 # init cmake
-def init_cmake(project_vars,websocket):
+async def init_cmake(project_vars,websocket):
     # get the project vars
+    Sysver = project_vars[0]
+    Project_name = project_vars[1]  
+    Entry_point = project_vars[2]
+    Output_Name = project_vars[3]
+    print("Project Name: " + Project_name + "\n")
+    print("Entry Point: " + Entry_point + "\n")
+    print("Output Name: " + Output_Name + "\n")
+    
+    Project_Build_System = project_vars[4]
+    # create the CMakeLists.txt file
+    with open(Project_name + "/CMakeLists.txt", "w") as f:
+        f.write("cmake_minimum_required(VERSION 3.10)\n")
+        f.write("project(" + Project_name + ")\n")
+        f.write("add_executable(" + Output_Name + " " + Entry_point + ")\n")
+    # create the build directory
+    os.system("mkdir " + Project_name + "/build")
+    print("CMakeLists.txt created\n")
+    print("Build directory created\n")
+    print("Running cmake\n")
+    print(Project_name)
+    print(Output_Name)
+    # run cmake
+    await execute_code("cmake -S " + Project_name + " -B " + Project_name + "/build", websocket)
+    # # run make
+    await execute_code("make -C " + Project_name + "/build", websocket)
+    # # run the output
+    await execute_code("./" + Project_name + "/build/" + Output_Name, websocket)    
+        
+async def Run_PMS_system(websocket, path,Project_name):
+    print("Running PMS System\n")
+    print("Path: " + path + "\n")
+    print("Websocket: " + str(websocket) + "\n")
+
+    # get the project vars
+    with open(Project_name + "/project_vars.json", "r") as f:
+        project_vars = json.loads(f.read())
     Sysver = project_vars[0]
     Project_name = project_vars[1]
     Entry_point = project_vars[2]
     Output_Name = project_vars[3]
     Project_Build_System = project_vars[4]
-    # create the CMakeLists.txt file
-    with open(Project_name + "/CMakeLists.txt", "w") as f:
-        f.write("cmake_minimum_required(VERSION 3.10)\n")
-        f.write("project(" + Project_name + " VERSION 1.0)\n")
-        f.write("add_executable(" + Output_Name + " " + Entry_point + ")\n")
-    # create the build directory
-    os.system("mkdir " + Project_name + "/build")
-    # run cmake
-    execute_code("cd " + Project_name + "/build && cmake ..", websocket)
-    # run make
-    execute_code("cd " + Project_name + "/build && make", websocket)
-    # run the program
-    execute_code("cd " + Project_name + "/build && ./" + Output_Name, websocket)
-        
-async def Run_PMS_system(websocket, path):
-    print("Running PMS System")
-    print("Path: " + path)
-    print("Websocket: " + str(websocket))
-    while True:
-        # get the project vars
-        with open(Project_name + "/project_vars.json", "r") as f:
-            project_vars = json.loads(f.read())
-        Sysver = project_vars[0]
-        Project_name = project_vars[1]
-        Entry_point = project_vars[2]
-        Output_Name = project_vars[3]
-        Project_Build_System = project_vars[4]
-        # run the PMS system
-        match Project_Build_System:
-            case "cmake":
-                init_cmake(project_vars,websocket)
-            case "make":
-                pass
-            case "ant":
-                pass
-            case "gradle":
-                pass
-            case "maven":
-                pass
-            case "npm":
-                pass
+    # run the PMS system
+    match Project_Build_System:
+        case "cmake":
+            await init_cmake(project_vars,websocket)
+        case "make":
+            pass
+        case "ant":
+            pass
+        case "gradle":
+            pass
+        case "maven":
+            pass
+        case "npm":
+            pass
+        case "yarn":    
+            pass    
+        case "pip":
+            pass
+        case "cargo":
+            pass
+        case "go":
+            pass
+        case "dotnet":
+            pass
+            
+            
             
 async def PMS(websocket, path):
     # main function for the PMS system
@@ -132,4 +191,9 @@ async def PMS(websocket, path):
     print("Path: " + path)
     print("Websocket: " + str(websocket))
     
-    while True:
+
+    code = await websocket.recv()
+    parsed_code = json.loads(code)
+    print("Code: " + str(parsed_code))
+    await Read_PMS_File(websocket,path, code)
+        
--- runner/lib/server.py
+++ runner/lib/server.py
@@ -68,23 +68,14 @@ async def handler(websocket, path):
             await PythonLSP(websocket, path)
         case "/cpp":
             await CPP(websocket, path)
-      
         case "/py":
           await server(websocket, path)
-        
         case "/help":
             await websocket.send(conf.help)
-        case "/test":
-            websocket.send("starting test")
-            data = PMSsystem.PMSSystem.assign(await websocket.recv())
-            print(data)
-            await websocket.send(data)
         case "/PMS":
-            await PMSsystem.PMSSystem.PMS(websocket, path)
-
+            await PMSsystem.PMS(websocket, path)
         case "/code":
-            await PMSsystem.PMSSystem.PMSCode(websocket, path)
-
+            await PMSsystem. Write_code_Buffer (websocket, path)
         case "/debug":
             de_bug(websocket, "Connected to debug server", "INFO")
             await debug(websocket, path)
@@ -96,6 +87,7 @@ async def handler(websocket, path):
                 # send the received code to all connected clients
                 for client in websockets:
                     await client.send(code)
+                    
         case "/request": await websocket.send(conf.endpoints)
         case _: await websocket.send("Invalid path")
 
@@ -103,14 +95,10 @@ async def handler(websocket, path):
 
     
 ws_server = websockets.serve(handler, conf.WS_HOST, conf.WS_PORT)
-
-
-
 print("CodeRunner Server version 2.0")
 print("TEST SERVER: Things may break")
 print("Server started at port", conf.WS_PORT)
 print("Relay started at address https://localhost:5000/")
 print("Press Ctrl+C to stop the server")
 asyncio.get_event_loop().run_until_complete(ws_server)
-
 asyncio.get_event_loop().run_forever()
