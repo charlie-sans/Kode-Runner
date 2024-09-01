8f4d79704583064ac8ef86df8785c140e8eecb4a
charliesan <charliesan@envs.net>
2024-09-01 16:45:46 +1000

    fixed issues with coderunner not working on certain sytems

--- runner/start.sh
+++
@@ -1,6 +0,0 @@
-docker network create --opt com.docker.network.driver.mtu=1500 my-restricted-network
-docker network policy create --ingress --from none my-restricted-policy
-docker network policy rule add my-restricted-policy --from my-restricted-network --to any --port any --protocol tcp --action deny
-docker network policy rule add my-restricted-policy --from my-restricted-network --to any --port any --protocol udp --action deny
-docker network policy rule add my-restricted-policy --from my-restricted-network --to any --port 5000 --protocol tcp --action allow  # Allow access to localhost port 5000 (HTTP)
-
