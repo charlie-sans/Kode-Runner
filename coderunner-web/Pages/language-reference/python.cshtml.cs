a10d466e70696fd4cfb52dc766aa583951513b72
charliesan <charliesan@envs.net>
2024-09-06 10:27:05 +1000

    Refactor code to remove unnecessary files and functions and to figure out why the dotnet site host doesn't want to compile and how i should even add sub pages to the cunt

---
+++ coderunner-web/Pages/language-reference/python.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace coderunner_web.Pages.language_reference.PythonModel
+{
+    public class CatModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
