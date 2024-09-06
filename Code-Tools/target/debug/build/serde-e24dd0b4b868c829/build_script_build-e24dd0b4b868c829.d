035d4a2bc51ec7f526562308fed3c24a199d5a1e
charliesan <charliesan@envs.net>
2024-09-07 05:59:55 +1000

    Refactor code to remove unnecessary files and functions

--- Code-Tools/target/debug/build/serde-e24dd0b4b868c829/build_script_build-e24dd0b4b868c829.d
+++
@@ -1,5 +0,0 @@
-/home/charlie/git/CodeRunner-projects/Code-Tools/target/debug/build/serde-e24dd0b4b868c829/build_script_build-e24dd0b4b868c829: /home/charlie/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.209/build.rs
-
-/home/charlie/git/CodeRunner-projects/Code-Tools/target/debug/build/serde-e24dd0b4b868c829/build_script_build-e24dd0b4b868c829.d: /home/charlie/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.209/build.rs
-
-/home/charlie/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.209/build.rs:
